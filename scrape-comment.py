fd4dc133ca219681fcab19282c75c7500f333e88
Jenny Linh Nguyen <lnguyen@colgate.edu>
2021-03-31 01:36:29 -0400

    Finalize scrape-comments. Add first 4 months of 2020 to articles_by_month

--- scrape-comment.py
+++ scrape-comment.py
@@ -1,16 +1,24 @@
-import datetime as dt
 import pandas as pd
 import sys
 from nytimes_scraper.nyt_api import NytApi
 from nytimes_scraper.comments import fetch_comments, comments_to_df
 
+## Run the command as follow to scrape the number of comments per article 
+## python3 scrape-comment.py <year> <month> <api_key>
+
+## Example: 
+## python3 scrape-comment.py 2020 12 svVK2Twxudkfd3IxQozMz3NyyjvsVNmZ
+
+## Articles of the specified month must be scraped and stored at the directory articles_by_month/
+## Updated dataset (articles + n_comments) of the specified month
+## is stored at the directory updated_articles_by_month/
+
 year = sys.argv[1]
 month = sys.argv[2]
 api_key = sys.argv[3]
 
-# date = dt.date(int(year), int(month), 1)
-name = 'article_' + year + '_' + month + '.csv'
-updated_name = 'updated_article_' + year + '_' + month + '.csv'
+name = './articles_by_month/article_' + year + '_' + month + '.csv'
+updated_name = './updated_articles_by_month/updated_article_' + year + '_' + month + '.csv'
 
 api = NytApi(api_key)
 
@@ -18,18 +26,21 @@ print(updated_name)
 print('API KEY: ' + api_key)
 
 # Fetch articles of a specific month
-article_df = pd.read_csv('./articles_by_month/' + name)
+article_df = pd.read_csv(name)
 article_ids_and_urls = list(article_df['web_url'].iteritems())
 
 comments = fetch_comments(api, article_ids_and_urls)
-comment_df = comments_to_df(comments)
+comments_df = comments_to_df(comments)
 
 # Get the number of comments per article
 comment_series = comments_df['articleID'].value_counts().to_frame().reset_index()
-comment_series.columns=['_id', 'n_comments']
+comment_series.columns=['_temp_id', 'n_comments']
+article_df = article_df.rename(columns={'Unnamed: 0' : '_temp_id'})
 
 # Merge to get the updated article df 
-updated_df = articles_df.merge(comment_series, on=['_id'])
-  
+updated_df = pd.merge(article_df, comment_series, on='_temp_id', how='outer')
+updated_df = updated_df.drop(['_temp_id'], axis=1)
+updated_df['n_comments'] = updated_df['n_comments'].fillna(0)
+
 # Export to csv file 
 updated_df.to_csv(updated_name)
